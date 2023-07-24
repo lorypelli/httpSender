c2c6906159627e988aec44af07b1e52b6de15efc
RVG|lory <87276663+lorypelli@users.noreply.github.com>
2023-07-24 17:26:16 +0000

    improvements

--- index.ts
+++ index.ts
@@ -10,14 +10,12 @@ export class httpSender {
             headers: { 'Authorization': this.authorization! || auth! }
         });
     }
-    async post(url: string, body: object, auth?: string) {
+    async post(url: string, body: object | string, auth?: string) {
         if (!this.authorization && !auth) return console.log('Authorization header is missing') as unknown as Response;
         return await fetch(url, {
             method: 'POST',
             headers: { 'Authorization': this.authorization! || auth! },
-            body: JSON.stringify({
-                ...body
-            })
+            body: JSON.stringify({...body as object} || body)
         });
     }
     async patch(url: string, body: object, auth?: string) {
@@ -25,9 +23,7 @@ export class httpSender {
         return await fetch(url, {
             method: 'PATCH',
             headers: { 'Authorization': this.authorization! || auth! },
-            body: JSON.stringify({
-                ...body
-            })
+            body: JSON.stringify({...body as object} || body)
         });
     }
     async put(url: string, body: object, auth?: string) {
@@ -35,9 +31,7 @@ export class httpSender {
         return await fetch(url, {
             method: 'PUT',
             headers: { 'Authorization': this.authorization! || auth! },
-            body: JSON.stringify({
-                ...body
-            })
+            body: JSON.stringify({...body as object} || body)
         });
     }
     async delete(url: string, body?: object, auth?: string) {
@@ -45,12 +39,13 @@ export class httpSender {
         return await fetch(url, {
             method: 'DELETE',
             headers: { 'Authorization': this.authorization! || auth! },
-            body: JSON.stringify({
-                ...body
-            })
+            body: JSON.stringify({...body as object} || body)   
         });
     }
 }
 export async function toJSON(res: Response) {
     return await res.json();
+}
+export async function toText(res: Response) {
+    return await res.text();
 }
