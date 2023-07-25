78d5713e0b137719ec67133cb88f96602a288c9c
RVG|lory <87276663+lorypelli@users.noreply.github.com>
2023-07-25 07:25:17 +0000

    fix

--- index.ts
+++ index.ts
@@ -4,9 +4,12 @@ export class httpSender {
     constructor(options?: Optios) {
         this.options = options;
     }
-    async get(url: string, ctype: string = 'application/json', auth?: string) {
-        if (!this.options) return await fetch(url, {
+    async get(url: string, auth?: string, ctype: string = 'application/json') {
+        if (!this.options && !auth) return await fetch(url, {
             headers: { 'Content-type': ctype }
+        });
+        else if (!this.options) return await fetch(url, {
+            headers: { 'Authorization': auth!, 'Content-type': ctype }
         });
         else if (!this.options!.authorization && !auth) return await fetch(url, {
             headers: { 'Content-type': this.options!.content_type! || ctype }
@@ -15,72 +18,92 @@ export class httpSender {
             headers: { 'Authorization': auth! || this.options!.authorization!, 'Content-type': this.options!.content_type! || ctype }
         });
     }
-    async post(url: string, ctype: string = 'application/json', body: object | string, auth?: string) {
-        if (!this.options) return await fetch(url, {
+    async post(url: string, body: object | string, auth?: string, ctype: string = 'application/json') {
+        if (!this.options && !auth) return await fetch(url, {
             method: 'POST',
             headers: { 'Content-type': ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
-        if (!this.options!.authorization && !auth) return await fetch(url, {
+        else if (!this.options) return await fetch(url, {
+            method: 'POST',
+            headers: { 'Authorization': auth!, 'Content-type': ctype },
+            body: JSON.stringify({ ...body as object } || body)
+        });
+        else if (!this.options!.authorization && !auth) return await fetch(url, {
             method: 'POST',
             headers: { 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
         return await fetch(url, {
             method: 'POST',
             headers: { 'Authorization': auth! || this.options!.authorization!, 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
     }
-    async patch(url: string, ctype: string = 'application/json', body: object, auth?: string) {
-        if (!this.options) return await fetch(url, {
+    async patch(url: string, body: object | string, auth?: string, ctype: string = 'application/json') {
+        if (!this.options && !auth) return await fetch(url, {
             method: 'PATCH',
             headers: { 'Content-type': ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
-        if (!this.options!.authorization && !auth) return await fetch(url, {
+        else if (!this.options) return await fetch(url, {
+            method: 'PATCH',
+            headers: { 'Authorization': auth!, 'Content-type': ctype },
+            body: JSON.stringify({ ...body as object } || body)
+        });
+        else if (!this.options!.authorization && !auth) return await fetch(url, {
             method: 'PATCH',
             headers: { 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
         return await fetch(url, {
             method: 'PATCH',
             headers: { 'Authorization': auth! || this.options!.authorization!, 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
     }
-    async put(url: string, ctype: string = 'application/json', body: object, auth?: string) {
-        if (!this.options) return await fetch(url, {
+    async put(url: string, body: object | string, auth?: string, ctype: string = 'application/json') {
+        if (!this.options && !auth) return await fetch(url, {
             method: 'PUT',
             headers: { 'Content-type': ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
-        if (!this.options!.authorization && !auth) return await fetch(url, {
+        else if (!this.options) return await fetch(url, {
+            method: 'PUT',
+            headers: { 'Authorization': auth!, 'Content-type': ctype },
+            body: JSON.stringify({ ...body as object } || body)
+        });
+        else if (!this.options!.authorization && !auth) return await fetch(url, {
             method: 'PUT',
             headers: { 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
         return await fetch(url, {
             method: 'PUT',
             headers: { 'Authorization': auth! || this.options!.authorization!, 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
     }
-    async delete(url: string, ctype: string = 'application/json', body?: object, auth?: string) {
-        if (!this.options) return await fetch(url, {
+    async delete(url: string, body?: object | string, auth?: string, ctype: string = 'application/json') {
+        if (!this.options && !auth) return await fetch(url, {
             method: 'DELETE',
             headers: { 'Content-type': ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
-        if (!this.options!.authorization && !auth) return await fetch(url, {
+        else if (!this.options) return await fetch(url, {
+            method: 'DELETE',
+            headers: { 'Authorization': auth!, 'Content-type': ctype },
+            body: JSON.stringify({ ...body as object } || body)
+        });
+        else if (!this.options!.authorization && !auth) return await fetch(url, {
             method: 'DELETE',
             headers: { 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)
+            body: JSON.stringify({ ...body as object } || body)
         });
         return await fetch(url, {
             method: 'DELETE',
             headers: { 'Authorization': auth! || this.options!.authorization!, 'Content-type': this.options!.content_type! || ctype },
-            body: JSON.stringify({...body as object} || body)   
+            body: JSON.stringify({ ...body as object } || body)
         });
     }
 }
