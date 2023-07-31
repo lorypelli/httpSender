595e3d7ce99d841862e6e27eb37cbd5b9c65aede
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2023-07-31 10:31:25 +0200

    fix

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export class httpSender {
-    options?: Options;
+    private options?: Options;
     constructor(options?: Options) {
         this.options = options;
     }
